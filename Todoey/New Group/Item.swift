6b7e53e1789fe1453e0b79f6db139a511283491a
Sherry-dong <dclj19962@126.com>
2018-05-24 10:46:08 -0400

    convert to MVC design pattern.

---
+++ Todoey/New Group/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Chenlijun Dong on 5/22/18.
+//  Copyright © 2018 Chenlijun Dong. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
